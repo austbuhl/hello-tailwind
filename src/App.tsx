3696973004cea8a90db747e317bdec328be0028e
AB <austinbuhler@gmail.com>
2021-03-20 16:15:00 -0500

    import cx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
+import cx from 'classnames'
 
 function App() {
+  const [toggle, setToggle] = useState<boolean>(false)
+
+  const buttonClasses = cx({
+    'bg-red-500 hover:bg-red-500': !toggle,
+    'bg-blue-500 hover:bg-blue-500': toggle
+  })
   return (
     <div className='bg-gray-200 flex items-center justify-center h-screen'>
       <button
-        className='p-3 rounded-sm bg-blue-500 hover:bg-blue-700'
-        // onClick={() => setToggle(!toggle)}
+        className={`p-3 rounded-sm ${buttonClasses}`}
+        onClick={() => setToggle(!toggle)}
       >
         Toggle
       </button>
